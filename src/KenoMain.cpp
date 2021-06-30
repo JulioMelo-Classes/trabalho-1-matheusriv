9e5195e0a8a65f320942479eb481ae054f3c71f3
Matheus <matheus.r.slv@gmail.com>
2021-06-30 19:09:41 +0000

    mudancas iniciais no Main

--- src/KenoMain.cpp
+++ src/KenoMain.cpp
@@ -12,19 +12,39 @@
 
 #include <iostream>
 #include <algorithm>
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-
-using namespace std;
+#include <fstream>
+#include <sstream>
 
 #include "KenoBet.hpp"
+#include "OtherFunctions.hpp"
+
+using namespace std;
 
 //! The main function
 //! @param argc - number of command line arguments
 //! @param argv - list of command-line arguments, used to read the bet    file name
 //! @return 0 if success, 1 if not
 int main(int argc, char *argv[]) {
+    string aposta_filename;
+
+    if(argc == 1){ 
+        cerr << "Sem arquivo, jogando com o arquivo default de apostas" << endl;
+        aposta_filename = "bet_01.dat";
+    }
+    else
+        aposta_filename = argv[1];
+
+    cout << ">>> Lendo arquivo de apostas [data/" << aposta_filename << "], por favor aguarde..." << endl;
+    cout << "----------------------------------------------" << endl;
+
+    ifstream arqDados("data/"+aposta_filename);
+    if(arqDados.bad()) {
+        cerr << "ERRO, o arquivo não foi aberto" << endl;
+        exit(1);
+    }
+    cout << ">>> Aposta lida com sucesso!" << endl;
+
 
 }
