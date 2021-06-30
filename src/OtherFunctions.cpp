5aca05307b983faa367c71054d0e9fa3467bdb0f
Matheus <matheus.r.slv@gmail.com>
2021-06-30 04:33:50 +0000

    edit include  OtherFunctions

--- src/OtherFunctions.cpp
+++ src/OtherFunctions.cpp
@@ -5,8 +5,8 @@
  *  Source with the other functions of the Keno Game
  */
 
-#ifndef HelperFunctions_hpp
-#define HelperFunctions_hpp
+#ifndef OtherFunctions_hpp
+#define OtherFunctions_hpp
 
 #include <string>
 
