f4cae7006d4eda8dc1b7732b3f157dafa3b2e8fe
Matheus <matheus.r.slv@gmail.com>
2021-07-02 02:13:40 +0000

    add quick sort em OtherFunctions

--- include/OtherFunctions.hpp
+++ include/OtherFunctions.hpp
@@ -2,7 +2,7 @@
  *  @file HelperFunctions.hpp
  *  @brief Headers of other functions
  *
- *  Header with the other functions of the Keno Game 
+ *  Header with the auxiliary functions of the Keno Game 
  */
 
 #ifndef OtherFunctions_hpp
@@ -10,5 +10,23 @@
 
 #include <string>
 
+/*! This function takes last element as pivot, places the pivot element at its correct position in sorted array, and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot 
+    @param arr Array to be divided, 
+    @param low Starting index, 
+    @param high Ending index  
+    @return True if the number is inside the array; false otherwise */
+int partition (int *arr, int low, int high);
 
-#endif
+/*! The main function that implements QuickSort 
+    @param arr Array to be sorted, 
+    @param low Starting index, 
+    @param high Ending index  */
+void quickSortAlgorithm(int *arr, int low, int high);
+
+/*! The quickSort friendly function to be called
+    @param arr The vector to be sorted
+    @param size The vector's size */
+void quickSort(int *arr, int size);
+
+
+#endif //OtherFunctions_hpp
--- src/OtherFunctions.cpp
+++ src/OtherFunctions.cpp
@@ -2,7 +2,7 @@
  *  @file OtherFunctions.cpp
  *  @brief General Functions
  *
- *  Source with the other functions of the Keno Game
+ *  Source with the auxiliary functions of the Keno Game
  */
 
 #include <string>
@@ -10,6 +10,36 @@
 
 #include "OtherFunctions.hpp"
 
+int partition (int *arr, int low, int high) { 
+    // pivot 
+    int pivot = arr[high]; 
+     // Index of smaller element and indicates the right position of pivot found so far
+    int i = (low - 1);
 
+    for(int j = low; j <= high - 1; j++) { 
+        // If current element is smaller than the pivot 
+        if(arr[j] < pivot){ 
+            i++; // increment index of smaller element 
+            std::swap(arr[i], arr[j]); 
+        } 
+    } 
+    std::swap(arr[i + 1], arr[high]); 
+    return (i + 1); 
+} 
 
-#endif
+void quickSortAlgorithm (int *arr, int low, int high) { 
+    if(low < high){ 
+        /* pi is partitioning index, arr[p] is now 
+        at right place */
+        int pi = partition(arr, low, high); 
+
+        // Separately sort elements before partition and after partition 
+        quickSortAlgorithm(arr, low, pi - 1); 
+        quickSortAlgorithm(arr, pi + 1, high); 
+    } 
+} 
+
+void quickSort (int *arr, int size){
+    quickSortAlgorithm(arr, 0, size-1);
+}
+
